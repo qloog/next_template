6de6a9a3690f681faa9bc91150fd2c895c5dce32
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-11-28 20:18:14 -0500

    doe

--- components/Hero.js
+++ components/Hero.js
@@ -35,7 +35,7 @@ const Hero = () => {
       </div>
       <div className="lg:w-full">
         <Image
-          src="https://unsplash.com/photos/people-sitting-on-chair-in-front-of-table-while-holding-pens-during-daytime-KdeqA3aTnBY"
+          src="https://images.unsplash.com/photos/people-sitting-on-chair-in-front-of-table-while-holding-pens-during-daytime-KdeqA3aTnBY"
           alt="Product demo"
           className="w-full"
           priority={true}
