821453cc7d6dc745a78a6a22b1a3bb586713eb79
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-11-28 20:36:51 -0500

    done?

--- components/Hero.js
+++ components/Hero.js
@@ -35,7 +35,7 @@ const Hero = () => {
       </div>
       <div className="lg:w-full">
         <Image
-          src="https://source.unsplash.com/IlUq1ruyv0Q"
+          src="https://images.unsplash.com/photo-1639322537231-2f206e06af84?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY4NDkyMDk5OA&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
           alt="Product demo"
           className="w-full"
           priority={true}
