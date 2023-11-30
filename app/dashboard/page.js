adb6abfbe726e20e05c1db3a25ba00fc97d8cc25
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-11-30 14:50:32 -0500

    p

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -10,7 +10,14 @@ export default async function Dashboard() {
     <main className="min-h-screen p-8 pb-24">
       <section className="max-w-xl mx-auto space-y-8">
         <ButtonAccount />
-        <h1 className="text-3xl md:text-4xl font-extrabold">Thank You for joining the waitlist!</h1>
+        <h1 className="text-3xl md:text-4xl font-extrabold">Thank You for Signing Up!</h1>
+        <P>We're so excited to have you here. As of now, we are currently working on the Findrr App
+          and we would love to hear your feedback. You can email us at Rajvirnahar6667@gmail.com.
+          We will keep you updated on app updates, the final launch date and more features. 
+          When the app is launched, you will be one of the few hundreds of people to get access to 
+          these features. Stay tuned for more!
+
+        </P>
       </section>
     </main>
   );
