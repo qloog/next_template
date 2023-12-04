a5e43b034d97f46124c4a70a3c88f794e3e9001e
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-04 14:27:06 -0500

    email for now

--- config.js
+++ config.js
@@ -72,11 +72,11 @@ const config = {
   },
   mailgun: {
     // subdomain to use when sending emails, if you don't have a subdomain, just remove it. Highly recommended to have one (i.e. mg.yourdomain.com or mail.yourdomain.com)
-    subdomain: "mg",
+    subdomain: "rajvirnahar6667@gmail.com",
     // REQUIRED — Email 'From' field to be used when sending magic login links
-    fromNoReply: `Findrr <noreply@mg.findrr>`,
+    fromNoReply: `rajvirnahar6667@gmail.com`,
     // REQUIRED — Email 'From' field to be used when sending other emails, like abandoned carts, updates etc..
-    fromAdmin: `Rajvir at Findrr <Rajvir@mg.findrr>`,
+    fromAdmin: `rajvirnahar6667@gmail.com`,
     // Email shown to customer if need support. Leave empty if not needed => if empty, set up Crisp above, otherwise you won't be able to offer customer support."
     supportEmail: "rajvirnahar6667@gmail.com",
     // When someone replies to supportEmail sent by the app, forward it to the email below (otherwise it's lost). If you set supportEmail to empty, this will be ignored.
