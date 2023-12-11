07dbcaa83c6973d3535c93c11b402724d7f60218
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-11 13:10:58 -0500

    ko

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 
 import Hero from "@/components/Hero";
-import Pricing from "@/components/Pricing";
 import FAQ from "@/components/FAQ";
 import CTA from "@/components/CTA";
 import Footer from "@/components/Footer";
@@ -14,7 +13,6 @@ export default function Home() {
     
       <main className="bg-black text-white">
         <Hero />
-        <Pricing />
         <FAQ />
         <CTA />
       </main>
