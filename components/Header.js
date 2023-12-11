f9227bc78ca70310f8fbd601d8310d39ebcea1d3
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-11 12:06:06 -0500

    done

--- components/Header.js
+++ components/Header.js
@@ -44,12 +44,63 @@ const Header = () => {
         aria-label="Global"
       >  
         {/* Your logo/name on large screens */}
-        
+        <div className="flex lg:flex-1"> 
+          <Link
+            className="flex items-center gap-2 shrink-0 "
+            href="/"
+            title={`${config.appName} hompage`}
+          >   
+            <Image
+              //src={logo}
+              //alt={`${config.appName} logo`}
+              className="w-8"
+              placeholder="blur"
+              priority={true}
+              width={32}
+              height={32}
+            />
+          
+            <span className="font-extrabold text-lg">{config.appName}</span>
+          </Link>
+        </div>
         {/* Burger button to open menu on mobile */}
-        
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+            onClick={() => setIsOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 text-base-content"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+              />
+            </svg>
+          </button>
+        </div>
 
         {/* Your links on large screens */}
-       
+        <div className="hidden lg:flex lg:justify-center lg:gap-12 lg:items-center">
+          {links.map((link) => (
+            <Link
+             // href={link.href}
+              //key={link.href}
+              className="link link-hover"
+              //title={link.label}
+            >
+              {link.label}
+            </Link>
+          ))}
+        </div>
 
         {/* CTA on large screens */}
         <div className="hidden lg:flex lg:justify-end lg:flex-1">{cta}</div>
@@ -61,7 +112,45 @@ const Header = () => {
           className={`fixed inset-y-0 right-0 z-10 w-full px-8 py-4 overflow-y-auto bg-base-200 sm:max-w-sm sm:ring-1 sm:ring-neutral/10 transform origin-right transition ease-in-out duration-300`}
         >
           {/* Your logo/name on small screens */}
-          
+          <div className="flex items-center justify-between">
+            <Link
+              className="flex items-center gap-2 shrink-0 "
+              title={`${config.appName} hompage`}
+              href="/"
+            >
+              <Image
+                src={logo}
+                alt={`${config.appName} logo`}
+                className="w-8"
+                placeholder="blur"
+                priority={true}
+                width={32}
+                height={32}
+              />
+              <span className="font-extrabold text-lg">{config.appName}</span>
+            </Link>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5"
+              onClick={() => setIsOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </div>
 
           {/* Your links on small screens */}
           <div className="flow-root mt-6">
