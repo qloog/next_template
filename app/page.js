93432a01e7dab3be377fe76cbd74098f12b2db73
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-11 13:02:36 -0500

    lol

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-      <Header />
+    
       <main className="bg-black text-white">
         <Hero />
         <Pricing />
