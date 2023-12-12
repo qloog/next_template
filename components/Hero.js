af88e1546dbe821a153e883fa37fdd2f2f65ba03
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-12 12:23:23 -0500

    .

--- components/Hero.js
+++ components/Hero.js
@@ -35,7 +35,8 @@ function Hero() {
     
       <backgroundImage
        src="https://images.unsplash.com/photo-1586243287039-23f4c8e2e7ab?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTcwMjE0MzY5Mg&ixlib=rb-4.0.3&q=80&utm_campaign=api-credit&utm_medium=referral&utm_source=unsplash_source&w=1080"
-
+       layout='fill'
+       object-fit='cover'
        />
 
       
