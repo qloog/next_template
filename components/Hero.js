2bd907bcd1d48ad3561ab94515b904c85398d720
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-12 12:53:44 -0500

    .

--- components/Hero.js
+++ components/Hero.js
@@ -68,5 +68,6 @@ function Hero() {
     </section>
   );
 }
-//src="https://images.unsplash.com/photo-1571171637578-41bc2dd41cd2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3540&q=80"
+
+
 export default Hero;
---
+++ components/Upload.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const ImageUpload = () => {
+    const [selectedFile, setSelectedFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setSelectedFile(event.target.files[0]);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (!selectedFile) {
+            alert('Please select a file first!');
+            return;
+        }
+
+        const formData = new FormData();
+        formData.append('file', selectedFile);
+
+        try {
+            const response = await fetch('/api/upload', {
+                method: 'POST',
+                body: formData,
+            });
+
+            if (response.ok) {
+                alert('Image uploaded successfully');
+            } else {
+                alert('Upload failed');
+            }
+        } catch (error) {
+            console.error('Error during the upload:', error);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type="file" onChange={handleFileChange} />
+            <button type="submit">Upload</button>
+        </form>
+    );
+};
+
+export default ImageUpload;
