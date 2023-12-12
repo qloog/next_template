40bb277f6d5f45c59dd97d480eb603cbc20756d4
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-12 15:13:26 -0500

    ?

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 
 import Hero from "@/components/Hero";
+import Script from "next/script";
 
  
 
@@ -12,8 +13,9 @@ export default function Home() {
     <>
     
       <main className="bg-black text-white">
+        <Hero />
+        <Script defer src="../vendor/script.js" />
         
-        <script src="../vendor/script.js" defer></script>
       
       </main>
   
