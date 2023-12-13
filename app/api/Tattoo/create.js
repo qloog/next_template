ba0b33e05c01e9746a86349adca6831bea74d45e
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-13 13:22:08 -0500

    ?

--- app/api/Tattoo/create.js
+++ app/api/Tattoo/create.js
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 const openai = new OpenAI();
 
 
-
+main();
 
 
 export default async function handler(req, res) {
@@ -13,10 +13,9 @@ export default async function handler(req, res) {
           model: "gpt-3.5-turbo",
         });
       
-        console.log('completion', completion)
+        console.log('completion', completion.data.choices)
         console.log(completion.choices[0]);
       }
 
     res.status(200).json({ name: 'John Doe' })
 }
-main();
