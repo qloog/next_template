7b72854ff1e95d039ca327775a9c3caf9917cae7
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-14 13:36:27 -0500

    done?

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ export default function Home() {
       
         <Hero />
         <script src="../vendor/snow.js" defer></script>
-        
+        <script src="/app/main.js" defer/>
       
       </main>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
