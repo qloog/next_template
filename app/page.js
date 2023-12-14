81d8b197e73e033d7abcc3c0ece7dcaa98c268fe
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-14 12:26:18 -0500

    ?

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 
 import Hero from "@/components/Hero";
+import { Input } from "postcss";
 
 
 
@@ -16,10 +17,26 @@ export default function Home() {
       
         <Hero />
         <script src="../vendor/snow.js" defer></script>
-        <title>Drag and Drop File Upload</title>
+        
+        <Title>Drag and Drop File Upload</Title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <meta charset="utf-8"></meta>
+        <link rel="shortcut icon" href="/assets/favicon.ico"></link>
+	<link rel="stylesheet" href="./src/main.css"></link>
+        
+
+ 
+<body>
+	<div class="drop-zone">
+		<span class="drop-zone__prompt">Drop file here or click to upload</span>
+		<Input type="file" name="myFile" class="drop-zone__input"></Input> 
+	</div>
+
+	<script src="./src/main.js"></script>
+</body>
       
       </main>
-  
+   
     </>
   );
 }
