f3dd37f550b73816a874ccde2e12d855f2516e72
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-14 14:39:25 -0500

    ??

--- app/main.js
+++ app/main.js
@@ -10,28 +10,29 @@ useEffect(() => {
     const cancelBtn = document.querySelector("#cancel-btn i");
     const img = document.querySelector("img");
     let regExp = /[0-9a-zA-Z ]+$/;
-  async function defaultBtnActive(){
-      defaultBtn.click();
-    }
-    defaultBtn.addEventListener("change", function(){
-      const file = this.files[0];
-      if(file){
-        const reader = new FileReader();
-        reader.onload = function(){
-          const result = reader.result;
-          img.src = result;
-          wrapper.classList.add("active");
-        }
-        cancelBtn.addEventListener("click", function(){
-          img.src = "";
-          wrapper.classList.remove("active");
-        })
-        reader.readAsDataURL(file);
-      }
-      if(this.value){
-        let valueStore = this.value.match(regExp);
-        fileName.textContent = valueStore;
-      }
-    });
+  
   }, [])
 
+  function defaultBtnActive(){
+    defaultBtn.click();
+  }
+  defaultBtn.addEventListener("change", function(){
+    const file = this.files[0];
+    if(file){
+      const reader = new FileReader();
+      reader.onload = function(){
+        const result = reader.result;
+        img.src = result;
+        wrapper.classList.add("active");
+      }
+      cancelBtn.addEventListener("click", function(){
+        img.src = "";
+        wrapper.classList.remove("active");
+      })
+      reader.readAsDataURL(file);
+    }
+    if(this.value){
+      let valueStore = this.value.match(regExp);
+      fileName.textContent = valueStore;
+    }
+  });
