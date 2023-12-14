c2918f184829e04df3b29496441b376e09dc2fad
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-14 12:27:40 -0500

    wbu

--- app/page.js
+++ app/page.js
@@ -18,7 +18,7 @@ export default function Home() {
         <Hero />
         <script src="../vendor/snow.js" defer></script>
         
-        <Title>Drag and Drop File Upload</Title>
+        <title>Drag and Drop File Upload</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta charset="utf-8"></meta>
         <link rel="shortcut icon" href="/assets/favicon.ico"></link>
