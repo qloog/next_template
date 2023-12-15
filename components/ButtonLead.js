731ec10367f0a3cfab88aa42763684b31c48b92f
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-15 13:06:01 -0500

    d

--- components/ButtonLead.js
+++ components/ButtonLead.js
@@ -52,7 +52,7 @@ const ButtonLead = ({ extraStyle }) => {
       />
 
       <button
-        className="btn btn-primary btn-block bg-linear-gradient(135deg,#3a8ffe 0%,#9658fe 100%)"
+        className="btn btn-primary btn-block bg-black"
         type="submit"
         disabled={isDisabled}
       >
