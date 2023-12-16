6a01e0c98f0e27a896f8daab55a61246a3585df2
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 12:39:43 -0500

    bg

--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import ButtonLead from "./ButtonLead";
-  
+import ButtonGradient from "@/components/ButtonGradient";  
 function Hero() {
   return (
     <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
