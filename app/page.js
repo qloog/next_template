ea066e35b31672eda9719c655c0b1f5e6483d209
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 13:34:08 -0500

    d

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
       <main className="bg-black text-white">
         <Hero />
       </main>
- <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black text-#dcdcdc text-left">
+ <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black text-#dcdcdc">
 <h3>Your current design</h3>
 
 
