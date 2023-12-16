b788cb3a51ca549cd4006578298a5a3e27c06f4a
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 12:28:00 -0500

    now?

--- components/ButtonGradient.js
+++ components/ButtonGradient.js
@@ -2,7 +2,7 @@
 
 const ButtonGradient = ({ title = "Start using Findrr now", onClick = () => {} }) => {
   return (
-    <button className=" btn btn-primary btn-block btn btn-gradient animate-shimmer w-full max-w-xs space-y-3 " onClick={onClick}>
+    <button className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3 flex flex-col gap-10 lg:gap-14 items-center justify-center " onClick={onClick}>
       {title}
     </button>
   );
