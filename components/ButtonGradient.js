adbcc76a0d3cbca506830fa2ce5e45f3735e200a
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 12:01:49 -0500

    center?

--- components/ButtonGradient.js
+++ components/ButtonGradient.js
@@ -2,7 +2,7 @@
 
 const ButtonGradient = ({ title = "Start using Findrr now", onClick = () => {} }) => {
   return (
-    <button className="btn btn-gradient animate-shimmer" onClick={onClick}>
+    <button className="btn btn-gradient animate-shimmer items-center justify-center" onClick={onClick}>
       {title}
     </button>
   );
