f2c40145a7887c77a6d79531818326f3f34898b3
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 11:57:34 -0500

    ?

--- app/page.js
+++ app/page.js
@@ -1,13 +1,14 @@
 "use client";
 
 import Hero from "@/components/Hero";
-
+import ButtonGradient from "@/components/ButtonLead"
 
 export default function Home() {
   return (
     <>
       <main className="bg-black text-white">
         <Hero />
+        <ButtonGradient></ButtonGradient>
       </main>
 
       
