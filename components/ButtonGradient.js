3647e2975b1d4da6c168a94a2af971b2b00b9ec0
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 12:16:51 -0500

    no space

--- components/ButtonGradient.js
+++ components/ButtonGradient.js
@@ -2,7 +2,7 @@
 
 const ButtonGradient = ({ title = "Start using Findrr now", onClick = () => {} }) => {
   return (
-    <button className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3" onClick={onClick}>
+    <button className="btn btn-gradient animate-shimmer " onClick={onClick}>
       {title}
     </button>
   );
