dde6e03cd95ccea87386d84c128d2fb19dac9ed8
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 12:33:29 -0500

    a/

--- components/ButtonLead.js
+++ components/ButtonLead.js
@@ -51,7 +51,29 @@ const ButtonLead = ({ extraStyle }) => {
         onChange={(e) => setEmail(e.target.value)}
       />
 
-      
+      <ButtonGradient>
+        className=&quot;btn btn-gradient animate-shimmer&quot;
+        type=&quot;submit&quot;
+        disabled={isDisabled}
+    
+        Start using Findrr now
+        {isLoading ? (
+          <span className="loading loading-spinner loading-xs"></span>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            className="w-5 h-5"
+          >
+            <path
+              fillRule="evenodd"
+              d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z"
+              clipRule="evenodd"
+            />
+          </svg>
+        )}
+      </ButtonGradient>
     </form>
   );
 };
