75febd444776026c3b2a8e8094d37b43dec4d4cf
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 12:30:46 -0500

    ?

--- components/Hero.js
+++ components/Hero.js
@@ -29,7 +29,13 @@ function Hero() {
       
 
   
+    
+    <button className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3" onClick={onClick}>
+      {title}
+    </button>
 
+
+export default ButtonGradient;
       
 
       
