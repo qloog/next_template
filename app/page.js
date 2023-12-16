15adb090ed152cbea0e4b3fa645edfb9383ccceb
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 13:49:24 -0500

    h1

--- app/page.js
+++ app/page.js
@@ -10,9 +10,9 @@ export default function Home() {
         <Hero />
       </main>
  <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
-<h3 className="opacity-80 text-white">
+<h1 className="opacity-80 text-white">
   Your current design
-  </h3>
+  </h1>
 
 
 
