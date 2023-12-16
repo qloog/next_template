3972e9bf4e392100c1f754fed35b589d563a3645
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 11:59:51 -0500

    ff

--- components/ButtonGradient.js
+++ components/ButtonGradient.js
@@ -1,6 +1,6 @@
 "use client";
 
-const ButtonGradient = ({ title = "Gradient Button", onClick = () => {} }) => {
+const ButtonGradient = ({ title = "Start using Findrr now", onClick = () => {} }) => {
   return (
     <button className="btn btn-gradient animate-shimmer" onClick={onClick}>
       {title}
