c57445edca8a5a1bc7993a75b67915c96f2d84ae
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 12:50:00 -0500

    done

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import Hero from "@/components/Hero";
-import ButtonGradient from "@/components/ButtonLead"
+
 
 export default function Home() {
   return (
--- components/Hero.js
+++ components/Hero.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import ButtonLead from "./ButtonLead";
-  
+
 function Hero() {
   return (
     <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
@@ -10,46 +10,45 @@ function Hero() {
           target="_blank"
           className=" -mb-4 md:-mb-6 group"
           //title="Product Hunt link"
-        >  
-        </a>
+        ></a>
 
         <h1 className="font-sans-serif text-4xl lg:text-6xl tracking-tight md:-mb-4 text-white text-align:left">
-        Fire your tattoo consulter
+          Fire your tattoo consulter
         </h1>
         <p className="text-lg opacity-80 leading-relaxed text-white">
-        Design your own tattoo with the first AI Tattoo Consulter
+          Design your own tattoo with the first AI Tattoo Consulter
         </p>
-  
-      <p>Findrr is the first AI Tattoo Consulter in the world.</p>
-      <p>Upload photos of your own, and then use Findrr to transform it into a custom design. Get Tattoo design inspiration and new ideas for your tattoo.</p>
-       
-      <input 
-        required
-        type="email"
-        value={email}
-        ref={inputRef}
-        autoComplete="email"
-        placeholder="Type your email..."
-        className="input input-bordered w-full placeholder:opacity-60 bg-white"
-        onChange={(e) => setEmail(e.target.value)}
-      />
-       <button className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3">
-       Start using Findrr now
-       type="submit"
-       </button>
 
-       
-        </div>
+        <p>Findrr is the first AI Tattoo Consulter in the world.</p>
+        <p>
+          Upload photos of your own, and then use Findrr to transform it into a
+          custom design. Get Tattoo design inspiration and new ideas for your
+          tattoo.
+        </p>
+        <form
+          className={`w-full max-w-xs space-y-3 ${
+            extraStyle ? extraStyle : ""
+          }`}
+          onSubmit={handleSubmit}
+        >
+          <input
+            required
+            type="email"
+            value={email}
+            ref={inputRef}
+            autoComplete="email"
+            placeholder="Type your email..."
+            className="input input-bordered w-full placeholder:opacity-60 bg-white"
+            onChange={(e) => setEmail(e.target.value)}
+          />
 
-
-      
-
-      
-
-  
+          <button className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3">
+            Start using Findrr now type="submit"
+          </button>
+        </form>
+      </div>
     </section>
   );
 }
-
 
 export default Hero;
