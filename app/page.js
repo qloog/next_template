7d0380914cc59237622d3c5248f74de637e8863a
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-16 11:39:51 -0500

    done

--- app/page.js
+++ app/page.js
@@ -11,30 +11,7 @@ export default function Home() {
         <Hero />
       </main>
 
-      <head>
-        <meta charset="utf-8"></meta>
-        <link rel="stylesheet" href="main.css"></link>
-        <meta http-equiv="X-UA-Compatible" content="IE=7"></meta>
-        <meta
-          http-equiv="Content-Type"
-          content="text/html;charset=UTF-8"
-        ></meta>
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
-      </head>
-
-      <body>
-    <main>
-        <section class="droparea">
-            <i class="far fa-images"></i>
-            <p>Drop your .png or .jpg files here!</p>
-            <p><small>Up to 20 images, No max file size.</small></p>
-        </section>
-    </main>
-</body>
-
+      
     </>
   );
 }
