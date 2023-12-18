05f6690c84460c1e570676fb200e76d4369fd6de
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-18 15:29:27 -0500

    s

--- components/Textbox.js
+++ components/Textbox.js
@@ -2,19 +2,31 @@ import React, { useState } from 'react';
 
 const SimpleTextarea = () => {
     const [text, setText] = useState('');
+    const [idea, setIdea] = useState('');
 
     const handleChange = (e) => {
         setText(e.target.value);
     };
 
+    const handleButtonClick = () => {
+        // Here you can define what happens when the button is clicked
+        // For example, generating a new idea. For now, it just sets a placeholder text
+        setIdea('New idea generated!');
+    };
+
     return (
-        <textarea
-            value={text}
-            onChange={handleChange}
-            placeholder="Type here..."
-            rows={4}
-            cols={50}
-        />
+        <div>
+            <textarea
+                value={text}
+                onChange={handleChange}
+                placeholder="Type here..."
+                rows={4}
+                cols={50}
+            />
+            <br />
+            <button onClick={handleButtonClick}>Render new idea</button>
+            <p>{idea}</p>
+        </div>
     );
 };
 
