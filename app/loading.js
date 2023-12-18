d873a02a7cdf96fef352b678ecf92bee461ecb05
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-18 13:57:47 -0500

    done?

---
+++ app/loading.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="loader">
+        <div className="spinner"></div>
+        <span className='text-xl font-bold'>Please wait...</span>
+    </div>
+  )
+}
+
+export default Loading
