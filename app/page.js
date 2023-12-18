b9f432fbb922411e3d8ba20ec7d34d56d58d9f64
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-18 13:39:10 -0500

    this>

--- app/page.js
+++ app/page.js
@@ -22,7 +22,43 @@ export default function Home() {
 <h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
   Your Custom-made Design 
   </h3>
-  
+  <div className='space-y-5'>
+      <h1>AI Content Generator</h1>
+      <div className='flex justify-between gap-10 min-h-screen'>
+        <section className='w-1/3'>
+          <form onSubmit={handleSubmit}>
+            <div className='form-input'>
+              <label>Select a content type:</label>
+              <RadioButtonGroup
+                options={radioButtonOptions}
+                onChange={handleChange}
+                selectedOption={formData.contentType}
+              />
+            </div>
+            <div className="form-input">
+              <label htmlFor='prompt'>What do you want to create?</label>
+              <textarea
+                id='prompt'
+                name='prompt'
+                placeholder='Enter a brief description...'
+                value={formData.prompt}
+                onChange={handleChange}
+                required
+              ></textarea>
+            </div>
+            <button type='submit' className='btn' disabled={loading}>Generate</button>
+          </form>
+        </section>
+
+        <section className='w-2/3'>
+          <Output
+            contentType={formData.contentType}
+            results={results}
+            loading={loading}
+          />
+        </section>
+      </div>
+    </div>
  </section>
 
 
