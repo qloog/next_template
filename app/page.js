b10b40f351bc6e42b7d7c8ba738629532545401c
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-18 12:35:46 -0500

    fix?

--- app/page.js
+++ app/page.js
@@ -9,14 +9,7 @@ export default function Home() {
     <>
       <main className="bg-black text-white">
         <Hero />
-      </main>
- <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
-<h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
-  Your current design
-  </h3>
-
-  
-<html lang="en"></html>
+        <html lang="en"></html>
 <head><meta charset="UTF-8"></meta>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
 	<link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'></link>
@@ -38,6 +31,14 @@ export default function Home() {
 	</div>
 	
 </body>
+      </main>
+ <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
+<h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
+  Your current design
+  </h3>
+
+  
+
  </section>
 
 
