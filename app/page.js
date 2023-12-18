f88f8efcd5ecfc82f4cb11e931e178d64ab4d5ab
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-18 15:08:13 -0500

    done

--- app/page.js
+++ app/page.js
@@ -55,7 +55,45 @@ const HomePage = () => {
   };
 
 }
+<div className='space-y-5'>
+<h1>AI Content Generator</h1>
+<div className='flex justify-between gap-10 min-h-screen'>
+  <section className='w-1/3'>
+    <form onSubmit={handleSubmit}>
+      <div className='form-input'>
+        <label>Select a content type:</label>
+        <RadioButtonGroup
+          options={radioButtonOptions}
+          onChange={handleChange}
+          selectedOption={formData.contentType}
+        />
+      </div>
+      <div className="form-input">
+        <label htmlFor='prompt'>What do you want to create?</label>
+        <textarea
+          id='prompt'
+          name='prompt'
+          placeholder='Enter a brief description...'
+          value={formData.prompt}
+          onChange={handleChange}
+          required
+        ></textarea>
+      </div>
+      <button type='submit' className='btn' disabled={loading}>Generate</button>
+    </form>
+  </section>
 
+  <section className='w-2/3'>
+    <Output
+      contentType={formData.contentType}
+      results={results}
+      loading={loading}
+
+      
+    />
+  </section>
+</div>
+</div>
 
 
 export default function Home() {
@@ -65,6 +103,7 @@ export default function Home() {
         <Hero />
         
       </main>
+
  <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
 <h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
   Your Tattoo Idea
@@ -72,54 +111,14 @@ export default function Home() {
   <UploadForm></UploadForm>
  </section>
 
+
+
  <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
 <h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
   Your Custom-made Design 
   </h3>
-  
-  <div className='space-y-5'>
-      <h1>AI Content Generator</h1>
-      <div className='flex justify-between gap-10 min-h-screen'>
-        <section className='w-1/3'>
-          <form onSubmit={handleSubmit}>
-            <div className='form-input'>
-              <label>Select a content type:</label>
-              <RadioButtonGroup
-                options={radioButtonOptions}
-                onChange={handleChange}
-                selectedOption={formData.contentType}
-              />
-            </div>
-            <div className="form-input">
-              <label htmlFor='prompt'>What do you want to create?</label>
-              <textarea
-                id='prompt'
-                name='prompt'
-                placeholder='Enter a brief description...'
-                value={formData.prompt}
-                onChange={handleChange}
-                required
-              ></textarea>
-            </div>
-            <button type='submit' className='btn' disabled={loading}>Generate</button>
-          </form>
-        </section>
-
-        <section className='w-2/3'>
-          <Output
-            contentType={formData.contentType}
-            results={results}
-            loading={loading}
-          />
-        </section>
-      </div>
-    </div>
  </section>
 
-
-
-
-      
     </>
   );
 }
