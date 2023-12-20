e5728f5dc6e60ad1a8360122cfd36261221e2287
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-19 23:08:41 -0500

    done

--- app/api/openai/route.js
+++ app/api/openai/route.js
@@ -1,16 +1,13 @@
+import fs from "fs";
 import OpenAI from "openai";
 
 const openai = new OpenAI();
 
 async function main() {
-    const stream = await openai.chat.completions.create({
-        model: "gpt-4",
-        messages: [{ role: "user", content: "Say this is a test" }],
-        stream: true,
-    });
-    for await (const chunk of stream) {
-        process.stdout.write(chunk.choices[0]?.delta?.content || "");
-    }
-}
+  const image = await openai.images.createVariation({
+    image: fs.createReadStream("otter.png"),
+  });
 
+  console.log(image.data);
+}
 main();
