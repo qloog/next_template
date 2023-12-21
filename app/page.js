d000c2af224d552b63f800c729ccb4be3a8b354f
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-21 14:59:14 -0500

    ...

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState } from "react";
+import React, {useState} from 'react'; 
 import Hero from "@/components/Hero";
 import ImageCard from "@/components/ImageCard"
 import parsedData from "@/pages/api/generateImage/index"
