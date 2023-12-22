08c8d467577b6ba726be0e0cfb14b3ec67e19775
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-22 12:01:44 -0500

    done

--- components/ImageCard.js
+++ components/ImageCard.js
@@ -3,10 +3,25 @@ import React from 'react';
 const ImageCard = ({ imageUrl, description }) => {
     if (!imageUrl) return null;
 
+    const cardStyle = {
+        border: '1px solid rgba(255, 255, 255, 0.8)', // White border with 80% opacity
+        padding: '10px',
+        margin: '10px',
+        borderRadius: '8px',
+        textAlign: 'center',
+    };
+
+    const imageStyle = {
+        maxWidth: '100%',
+        maxHeight: '300px',
+        display: 'block',
+        margin: '0 auto',
+    };
+
     return (
-        <div style={{ border: '1px solid #ddd', padding: '10px', margin: '10px', borderRadius: '8px' }}>
-            <img src={imageUrl} alt="Generated Tattoo" style={{ maxWidth: '100%', maxHeight: '300px', display: 'block', margin: '0 auto' }} />
-            {description && <p style={{ textAlign: 'center' }}>{description}</p>}
+        <div style={cardStyle}>
+            <img src={imageUrl} alt="Generated Tattoo" style={imageStyle} />
+            {description && <p>{description}</p>}
         </div>
     );
 };
