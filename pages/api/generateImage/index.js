01215ecbaa62afbbf3e67f768aa7a62624020198
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-25 14:24:49 -0500

    ?

--- pages/api/generateImage/index.js
+++ pages/api/generateImage/index.js
@@ -2,9 +2,9 @@
 import { json } from "express";
 import OpenAI from "openai";
 
-
-
-const openai = new OpenAI();
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY
+});
 
 export default async function handler(req, res) {
     const { prompt } = req.body;
