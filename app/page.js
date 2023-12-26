3646b83922a8cf3072265a6315e1e10a48de73d8
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-26 14:15:30 -0500

    ?

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   async function onGenerate(e) {
     e.preventDefault();
     try {
-      const results = await fetch('/api/generateImage', {
+      const results = await fetch('https://www.findrr.ca', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ prompt: prompt })
