a3531725cfcae5066fefdba3d03cf6d208756609
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-26 14:10:48 -0500

    ...

--- pages/api/generateImage/index.js
+++ pages/api/generateImage/index.js
@@ -7,8 +7,15 @@ const openai = new OpenAI({
 });
 
 export default async function handler(req, res) {
+    if (req.method !== 'POST') {
+        res.setHeader('Allow', ['POST']);
+        return res.status(405).json({ error: 'Method Not Allowed' });
+    }
+
     const { prompt } = req.body;
     console.log(req.body);
+
+    
     if (!prompt) {
         return res.status(400).json({ error: "Prompt is required" });
     }
