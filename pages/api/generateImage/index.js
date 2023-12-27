d216c475ecd0b63a75a7b37ed2a3484358e1b138
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-27 13:53:50 -0500

    sjdjf

--- pages/api/generateImage/index.js
+++ pages/api/generateImage/index.js
@@ -1,16 +1,12 @@
-
+import { json } from "express";
 import OpenAI from "openai";
 
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
-});
-
+const openai = new OpenAI
    
 
 
 export default async function handler(req, res) {
-    
     if (req.method !== 'POST') {
         res.setHeader('Allow', ['POST']);
         return res.status(405).json({ error: 'Method Not Allowed' });
@@ -23,7 +19,7 @@ export default async function handler(req, res) {
     if (!prompt) {
         return res.status(400).json({ error: "Prompt is required" });
     }
-    
+    try {
         const image = await openai.images.generate({
             model: "dall-e-3", 
             prompt: prompt});
@@ -33,10 +29,12 @@ export default async function handler(req, res) {
          
    
        res.status(200).json({ imageUrl, finalData })
+    }
+    catch (error) {
+        console.error('OpenAI API Error:', error);
+        res.status(500).json({ error: 'Error generating image', details: error.message });
+    }
+    
+
   
 }
-
-
-
-
-
