35512edda474c2139671b466f577dd2929e1bc0a
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-27 15:03:52 -0500

    n

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default function Home() {
     try {
       const results = await fetch('/api/generateImage', {
         method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
+        headers: { 'Content-Type': 'application/json' }.then(r => r.json()),
         body: JSON.stringify({ prompt: prompt })
         
       }).then(r => r.json());
