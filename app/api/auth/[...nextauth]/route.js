699dc5f312ec1cb963309f8f9f7ca5969599e60f
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-27 15:29:07 -0500

    done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,7 @@
-export default NextAuth({
-    providers: [
-      GoogleProvider({
-        clientId: process.env.GOOGLE_ID,
-        clientSecret: process.env.GOOGLE_SECRET,
-      }),
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-      async jwt({ token }) {
-        token.userRole = "user"
-        return token
-      },
-    },
-  })
+import NextAuth from "next-auth";
+import { authOptions } from "@/libs/next-auth";
+
+
+const handler = NextAuth(authOptions);
+
+export { handler as GET, handler as POST };
