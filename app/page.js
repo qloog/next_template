ad7483e265b4790920fa5a848522839d608efcd5
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-27 13:45:21 -0500

    ??

--- app/page.js
+++ app/page.js
@@ -11,7 +11,7 @@ export default function Home() {
   async function onGenerate(e) {
     e.preventDefault();
   
-      const results = await fetch('/api/generateImage', {
+      const results = await fetch('/app/api/generateImage', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ prompt: prompt })
