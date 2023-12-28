1ddffbb91135b09097b2a7055bf820621a8d5a29
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-28 14:03:28 -0500

    done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,15 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/libs/next-auth";
 
-export default NextAuth({
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-      async jwt({ token }) {
-        token.userRole = "user"
-        return token
-      },
-    },
-  })
+
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
