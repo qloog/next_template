00ea9bebbe13e389537525ea482f19f13588d607
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-29 18:31:12 -0500

    mm

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
   const [prompt, setPrompt] = useState('');
   const [finalData, setFinalData] = useState();
   const [isButtonActive, setIsButtonActive] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   async function onGenerate(e) {
     e.preventDefault();
@@ -19,6 +20,7 @@ export default function Home() {
         
       }).then(r => r.json());
       setFinalData(results.imageUrl)
+      setIsLoading(false); // End loading
   }
 
   const textboxStyle = {
@@ -60,7 +62,7 @@ export default function Home() {
                 placeholder="Enter your tattoo idea" 
                 style={textboxStyle} 
             />
-        <button style={buttonStyle} onClick={onGenerate} onMouseDown={handleMouseDown}  onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp}>Render new Tattoo</button>
+        <button style={buttonStyle} onClick={onGenerate} onMouseDown={handleMouseDown}  onMouseUp={handleMouseUp} onMouseLeave={handleMouseUp}>{isLoading ? "Loading..." : "Render new Tattoo"}</button>
         <GeneratedImageCard finalData={finalData} />
       </section>
       </main>
