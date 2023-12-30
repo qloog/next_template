7bc7338ab81b0243e8000f41ca4153e14655ef84
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2023-12-30 12:50:22 -0500

    kgkkg

--- next.config.js
+++ next.config.js
@@ -3,10 +3,12 @@ const nextConfig = {
   images: {
     domains: [
       // NextJS <Image> component needs to whitelist domains for src={}
+      'i.pinimg.com',
       "lh3.googleusercontent.com",
       "pbs.twimg.com",
       "images.unsplash.com",
       "logos-world.net",
+      "toppng.com",
     ],
   },
 };
