5f24d0ac0eeb696a984c12d8746b3b32e95edf23
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-02 14:01:57 -0500

    now?

--- components/Hero.js
+++ components/Hero.js
@@ -3,7 +3,9 @@ import ButtonLead from "./ButtonLead";
 import ButtonCheckout from "@/components/ButtonCheckout";
 
 function Hero() {
+  const [isUserSignedUp, setIsUserSignedUp] = useState(false);
   return (
+    
     <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
         <a
