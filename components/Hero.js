76e6361faa62a9116b70d8e4ec452ae11551ad7d
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-02 14:15:05 -0500

    d

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,6 @@ import ButtonCheckout from "@/components/ButtonCheckout";
 
 function Hero() {
   return (
-    
     <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
         <a
@@ -27,6 +26,10 @@ function Hero() {
           custom design. Get Tattoo design inspiration and new ideas for your
           tattoo.
         </p>
+        <ButtonLead className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3">
+        Start using Findrr
+        </ButtonLead>
+    
       </div>
     </section>
   );
