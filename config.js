774f018334d5d8ec194f43596d44220a19cae898
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-03 00:13:15 -0500

    lllll

--- config.js
+++ config.js
@@ -19,10 +19,7 @@ const config = {
     plans: [
       {
         // REQUIRED — we use this to find the plan in the webhook (for instance if you want to update the user's credits based on the plan)
-        priceId: 
-          process.env.NODE_ENV === "development"
-            ? "price_1ju5GD464564"
-            : "price_1OUIuVHa8820Uqe5xny0meUl",
+        priceId: "price_1OUIuVHa8820Uqe5xny0meUl",
         //  REQUIRED - Name of the plan, displayed on the pricing page
         name: "Starter",
         // A friendly description of the plan, displayed on the pricing page. Tip: explain why this plan and not others
