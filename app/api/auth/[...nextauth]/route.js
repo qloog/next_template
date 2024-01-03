dd6222a21c6d70745a2ae1e649a931c4ad5d62c6
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-03 13:23:58 -0500

    done

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,25 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/libs/next-auth";
+import Providers from "next-auth/providers";
 
+export default NextAuth({
+    providers: [
+      Providers.Email({
+        server: {
+          host: process.env.EMAIL_SERVER_HOST,
+          port: process.env.EMAIL_SERVER_PORT,
+          auth: {
+            user: process.env.EMAIL_SERVER_USER,
+            pass: process.env.EMAIL_SERVER_PASSWORD,
+          },
+        },
+        from: process.env.EMAIL_FROM,
+      }),
+      // ... other providers ...
+    ],
+    // ... other configuration options ...
+  });
+  
 
 const handler = NextAuth(authOptions);
 
