f9cc44d6f383610588a093491416c17750b429e8
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-02 19:12:38 -0500

    fix

--- components/Hero.js
+++ components/Hero.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import ButtonLead from "./ButtonLead";
 import React, { useState, useRef } from 'react'; 
 
-function Hero() {
+function Hero({ onUserSignUp }) {
   return (
     <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
@@ -26,7 +26,7 @@ function Hero() {
           custom design. Get Tattoo design inspiration and new ideas for your
           tattoo.
         </p>
-        <ButtonLead onUserSignUp={props.onUserSignUp} className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3">
+        <ButtonLead onUserSignUp={onUserSignUp}className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3">
         Start using Findrr
         </ButtonLead>
     
