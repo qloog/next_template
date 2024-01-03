f7fcdb2d66f16dc01c5b54ad80f6e4ae728588e8
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-03 17:34:15 -0500

    looll

--- components/Hero.js
+++ components/Hero.js
@@ -30,7 +30,7 @@ function Hero() {
           custom design. Get Tattoo design inspiration and new ideas for your
           tattoo.
         </p>
-        <SigninButton></SigninButton>
+        <SigninButton className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3"></SigninButton>
     
       </div>
     </section>
