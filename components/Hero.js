33735d8a87c67f8c8810a08c6b22adfb5253403d
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-04 12:49:40 -0500

    done

--- components/Hero.js
+++ components/Hero.js
@@ -9,6 +9,13 @@ const priceId = config.stripe.plans[0].priceId; // Assuming it's the first plan
 function Hero() {
   return (
     <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
+
+       style={{
+        backgroundImage: `url(${/.next/cache/images/Khalsafauj.jpeg})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }}
+
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
         <a
           //href="https://www.producthunt.com/posts/shipfast-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-shipfast&#0045;2"
