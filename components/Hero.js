078cf36dd78c9e60eb74a33b1dab8a15ec841a80
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-04 12:55:38 -0500

    done

--- components/Hero.js
+++ components/Hero.js
@@ -6,12 +6,14 @@ import config from '@/config';
 import SigninButton from "@/components/ButtonSignIn"
 const priceId = config.stripe.plans[0].priceId; // Assuming it's the first plan
 
+const backgroundImageUrl = '/images/Khalsa fauj.jpeg'; 
+
 function Hero() {
   return (
     <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
 
-       style={{
-        backgroundImage: `url(${/.next/cache/images/Khalsafauj.jpeg})`,
+     style={{
+        backgroundImage: `url(${backgroundImageUrl})`,
         backgroundSize: 'cover',
         backgroundPosition: 'center',
       }}
@@ -33,7 +35,8 @@ function Hero() {
 
         <p>
           Write your tattoo idea, and Findrr will transform it into a
-          custom design.
+          custom design. Get Tattoo design inspiration and new ideas for your
+          tattoo.
         </p>
         <SigninButton className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3"></SigninButton>
     
