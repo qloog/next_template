9a4d13e3594443914f8dfee6b45a6c8384cdda12
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-05 23:30:33 -0500

    done

--- components/Hero.js
+++ components/Hero.js
@@ -1,15 +1,12 @@
-
-import React, { useState, useRef } from 'react'; 
-import config from '@/config';
-import SigninButton from "@/components/ButtonSignIn"
-import Image from 'next/image'; 
+import React, { useState, useRef } from "react";
+import config from "@/config";
+import SigninButton from "@/components/ButtonSignIn";
+import Image from "next/image";
 const priceId = config.stripe.plans[0].priceId; // Assuming it's the first plan
 
 function Hero() {
   return (
-<section className="heroBackground max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
-
-
+    <section className="heroBackground max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
         <a
           //href="https://www.producthunt.com/posts/shipfast-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-shipfast&#0045;2"
@@ -26,29 +23,34 @@ function Hero() {
         </p>
 
         <p>
-          Write your tattoo idea, and Findrr will transform it into a
-          custom design. Get Tattoo design inspiration and new ideas for your
-          tattoo.
+          Write your tattoo idea, and Findrr will transform it into a custom
+          design. Get Tattoo design inspiration and new ideas for your tattoo.
         </p>
         <SigninButton className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3"></SigninButton>
-       
+
         <Image
           src="/media/newyorktimes Small.png"
           alt="NYT"
           priority={true}
           width={200}
           height={200}
-         />
-      
-      <Image
+        />
+
+        <Image
           src="/media/tech crunch.png"
-          alt="NYT"
+          alt="TC"
           priority={true}
           width={200}
           height={200}
-         />
-         
-         
+        />
+
+        <Image
+          src="/media/fastcompany.png"
+          alt="FC"
+          priority={true}
+          width={200}
+          height={200}
+        />
       </div>
     </section>
   );
