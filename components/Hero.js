978e18d9e0661a42a38e440a9b22bde445ffda03
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-06 00:18:28 -0500

    done

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ const priceId = config.stripe.plans[0].priceId; // Assuming it's the first plan
 
 function Hero() {
   return (
-    <section className="heroBackground max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
+    <section className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-16 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
         <a
           //href="https://www.producthunt.com/posts/shipfast-2?utm_source=badge-top-post-badge&utm_medium=badge&utm_souce=badge-shipfast&#0045;2"
@@ -14,6 +14,14 @@ function Hero() {
           className=" -mb-4 md:-mb-6 group"
           //title="Product Hunt link"
         ></a>
+        <Image
+          src="/media/Khalsa.png"
+          alt="NYT"
+          priority={true}
+          className="w-full"
+          height={500}
+          width={500}
+        />
 
         <h1 className="font-sans-serif text-4xl lg:text-6xl tracking-tight md:-mb-4 text-white text-align:left">
           Fire your tattoo Designer
@@ -28,15 +36,49 @@ function Hero() {
         </p>
         <SigninButton className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3"></SigninButton>
 
-
-        <div style={{ display: 'flex', justifyContent: 'space-around', alignItems: 'center'}}>
-    <Image src="/media/The-New-York-Times-logo-whiteWEB-1.png" alt="NYT" priority={true} width={75} height={75} />
-    <Image src="/media/tech crunch.png" alt="TC" priority={true} width={75} height={75} />
-    <Image src="/media/fastcompany.png" alt="FC" priority={true} width={75} height={75} />
-    <Image src="/media/stability.png" alt="ST.AI" priority={true} width={75} height={75} />
-    <Image src="/media/archdaily.png" alt="AD" priority={true} width={75} height={75} />
-</div>
-
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "space-around",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src="/media/The-New-York-Times-logo-whiteWEB-1.png"
+            alt="NYT"
+            priority={true}
+            width={75}
+            height={75}
+          />
+          <Image
+            src="/media/tech crunch.png"
+            alt="TC"
+            priority={true}
+            width={75}
+            height={75}
+          />
+          <Image
+            src="/media/fastcompany.png"
+            alt="FC"
+            priority={true}
+            width={75}
+            height={75}
+          />
+          <Image
+            src="/media/stability.png"
+            alt="ST.AI"
+            priority={true}
+            width={75}
+            height={75}
+          />
+          <Image
+            src="/media/archdaily.png"
+            alt="AD"
+            priority={true}
+            width={75}
+            height={75}
+          />
+        </div>
       </div>
     </section>
   );
