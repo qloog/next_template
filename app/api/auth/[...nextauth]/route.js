4429722e6591e960ff236a974c28fe0894f67620
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-05 20:55:29 -0500

    ..

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,25 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/libs/next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-
+providers: 
+    GoogleProvider({
+      // Follow the "Login with Google" tutorial to get your credentials
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+      
+    })
+    // Follow the "Login with Email" tutorial to set up your email server
+    // Requires a MongoDB database. Set MONOGODB_URI env variable.
+   // ...(connectMongo
+    //  ? [
+    //      EmailProvider({
+      //      server: process.env.EMAIL_SERVER,
+     //       from: config.mailgun.fromNoReply,
+      //    }),
+     //  ]
+    //  : []),
+  
 
 const handler = NextAuth(authOptions);
 
