1a1169d89513e36fc15001409311d46b016ec2ef
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-06 13:22:47 -0500

    done

--- app/page.js
+++ app/page.js
@@ -74,7 +74,8 @@ const buttonStyle1 = {
       <main className="bg-black text-white">
     <ButtonAccount></ButtonAccount>
       <Hero />
-        <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
+        <section id="tattoo-generator" className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
+          
         <h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
           Your Tattoo Idea
         </h3>
