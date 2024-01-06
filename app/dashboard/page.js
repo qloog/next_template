b78f81024b4e3bf94e9fc8e9924e6b793832ece0
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-06 12:42:12 -0500

    done

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,16 @@
+import { useRouter } from 'next/router';
 import ButtonAccount from "@/components/ButtonAccount";
 import ButtonCheckout from "@/components/ButtonCheckout";
-import config from "@/config";
-import ButtonPopover from "@/components/ButtonPopover";
+import { useRouter } from 'next/router';
 
 export const dynamic = "force-dynamic";
 
 export default async function Dashboard() {
+  const router = useRouter();
+
+  const goToHomePage = () => {
+    router.push('/');
+  };
   return (
     <main className="min-h-screen p-8 pb-24">
       <section className="max-w-xl mx-auto space-y-8">
@@ -15,7 +20,12 @@ export default async function Dashboard() {
           You have 3/3 credits left on your free trial
         </h1>
 
-        <ButtonPopover></ButtonPopover>
+        <button 
+          onClick={goToHomePage} 
+          className="btn btn-primary">
+          Start Designing
+        </button>
+        
         
       </section>
     </main>
