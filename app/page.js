d928071154100bd8fa79b581f9c385289911f498
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-05 20:49:11 -0500

    done

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import React, {useState} from 'react';
 import Hero from "@/components/Hero";
 import GeneratedImageCard from "@/components/ImageBox"
 import Footer from '@/components/Footer';
+import ButtonAccount from '@/components/ButtonAccount'
 
 export default function Home() {
   const [style, setStyle] = useState('tattoo');
@@ -71,6 +72,7 @@ const buttonStyle1 = {
   return (
     <>
       <main className="bg-black text-white">
+    <ButtonAccount></ButtonAccount>
       <Hero />
         <section className="max-w-7xl mx-auto bg-black flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-black">
         <h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
