4377c85ce1697c711b462bf333a90974812d5547
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-07 17:48:23 -0500

    mmm

--- models/User.js
+++ models/User.js
@@ -57,6 +57,5 @@ const userSchema = mongoose.Schema(
 // add plugin that converts mongoose to json
 userSchema.plugin(toJSON);
 
-export default mongoose.model('User', userSchema);
-
+export default mongoose.models.User || mongoose.model("User", userSchema);
 
