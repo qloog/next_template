fb463b4d8d6871a3f74169e4e0157b6d3b81b4d0
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-07 14:46:41 -0500

    done

--- components/Pricing.js
+++ components/Pricing.js
@@ -5,49 +5,48 @@ import ButtonCheckout from "./ButtonCheckout";
 // It's your Stripe config in config.js.stripe.plans[] that will be used to display the plans
 // <ButtonCheckout /> renders a button that will redirect the user to Stripe checkout called the /api/stripe/create-checkout API endpoint with the correct priceId
 const Pricing = () => {
-  return (
-    <section className="bg-base-200 overflow-hidden" id="pricing">
-      <div className="py-8 px-4">
-        <div className="flex flex-col text-center w-full mb-8">
-          <h2 className="font-bold text-2xl lg:text-4xl tracking-tight"></h2>
-        </div>
+    return (
+      <section className="bg-base-200 overflow-hidden" id="pricing">
+        <div className="py-8 px-4">
+          <div className="flex flex-col text-center w-full mb-8">
 
-        <div className="flex flex-col items-center justify-center gap-8">
-          {config.stripe.plans.map((plan) => (
-            <div
-              key={plan.priceId}
-              className="w-full max-w-2xl bg-base-100 p-6 rounded-lg shadow-md"
-            >
-              <div>
-                <p className="text-lg font-bold text-center">{plan.name}</p>
-                {plan.description && (
-                  <p className="text-sm text-center mt-2">{plan.description}</p>
+            <h2 className="font-bold text-2xl lg:text-4xl tracking-tight">
+            </h2>
+          </div>
+  
+          <div className="flex flex-col items-center justify-center gap-8">
+            {config.stripe.plans.map((plan) => (
+              <div key={plan.priceId} className="w-full max-w-2xl bg-base-100 p-6 rounded-lg shadow-md">
+                <div>
+                  <p className="text-lg font-bold text-center">{plan.name}</p>
+                  {plan.description && (
+                    <p className="text-sm text-center mt-2">
+                      {plan.description}
+                    </p>
+                  )}
+                </div>
+                <p className="text-3xl font-bold text-center my-4">
+                  ${plan.price}
+                </p>
+                {plan.features && (
+                  <ul className="space-y-2 text-sm">
+                    {plan.features.map((feature, i) => (
+                      <li key={i} className="text-center">
+                        {feature.name}
+                      </li>
+                    ))}
+                  </ul>
                 )}
+                <div className="mt-6 text-center">
+                  <ButtonCheckout className="bg-rgb(244, 68, 37)" priceId={plan.priceId} />
+                </div>
               </div>
-              <p className="text-3xl font-bold text-center my-4">
-                ${plan.price}
-              </p>
-              {plan.features && (
-                <ul className="space-y-2 text-sm">
-                  {plan.features.map((feature, i) => (
-                    <li key={i} className="text-center">
-                      {feature.name}
-                    </li>
-                  ))}
-                </ul>
-              )}
-              <div className="mt-6 text-center">
-                <ButtonCheckout
-                  priceId={plan.priceId}
-                  className="custom-button-checkout"
-                />
-              </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-  );
-};
-
-export default Pricing;
+      </section>
+    );
+  };
+  
+  export default Pricing;
+  
