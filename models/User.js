332c625a3c5e80c26a2d5616fe5959dae6f2df24
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-07 18:02:18 -0500

    kk

--- models/User.js
+++ models/User.js
@@ -1,53 +1,61 @@
-import mongoose from 'mongoose';
-import toJSON from './plugins/toJSON';
+import mongoose from "mongoose";
+import toJSON from "./plugins/toJSON";
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    trim: true,
+// USER SCHEMA
+const userSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      private: true,
+    },
+    image: {
+      type: String,
+    },
+    // Used in the Stripe webhook to identify the user in Stripe and later create Customer Portal or prefill user credit card details
+    customerId: {
+      type: String,
+      validate(value) {
+        return value.includes("cus_");
+      },
+    },
+    // Used in the Stripe webhook. should match a plan in config.js file.
+    priceId: {
+      type: String,
+      validate(value) {
+        return value.includes("price_");
+      },
+    },
+    // Used to determine if the user has access to the product—it's turn on/off by the Stripe webhook
+    hasAccess: {
+      type: Boolean,
+      default: false,
+    },
+    imageCount: {
+      type: Number,
+      default: 0,
+    },
+    planType: {
+      type: String,
+      enum: ['beginner', 'veteran', 'premium'],
+      default: 'beginner',
+    },
   },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: true
-  },
-  image: {
-    type: String,
-  },
-  customerId: {
-    type: String,
-    validate(value) {
-      return value.startsWith('cus_');
-    }
-  },
-  priceId: {
-    type: String,
-    validate(value) {
-      return value.startsWith('price_');
-    }
-  },
-  hasAccess: {
-    type: Boolean,
-    default: false
-  },
-  imageCount: {
-    type: Number,
-    default: 0
-  },
-  planType: {
-    type: String,
-    enum: ['beginner', 'veteran', 'premium'],
-    default: 'beginner'
+
+  
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
   }
-}, {
-  timestamps: true,
-  toJSON: { virtuals: true }
-});
+);
 
-// Add plugin that converts mongoose to json
+// add plugin that converts mongoose to json
 userSchema.plugin(toJSON);
 
-const User = mongoose.model('User', userSchema);
-export default User;
+export default mongoose.models.User || mongoose.model("User", userSchema);
+
