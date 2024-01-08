52287a3212bbee01bffc7971096a28d566e53abe
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-08 14:44:50 -0500

    done

---
+++ app/api/generateImage/route.js
@@ -0,0 +1,51 @@
+import { NextResponse } from 'next/server'
+import OpenAI from 'openai'
+import { getServerSession } from 'next-auth/next'
+import { authOptions } from '../auth/[...nextauth]'
+import connectMongo from '@/libs/mongoose'
+import User from '@/models/User'
+
+export async function POST(req) {
+  const openai = new OpenAI()
+
+  const session = await getServerSession(authOptions)
+
+  const { id } = session.user
+
+  await connectMongo()
+
+  const user = await User.findById(id)
+
+  if (!user || user.currentCredits === 0) {
+    return NextResponse.json({ error: 'Not enough credits' }, { status: 403 })
+  }
+
+  const body = await req.json()
+
+  if (!body.prompt) {
+    return NextResponse.json({ error: 'Prompt is required' }, { status: 400 })
+  }
+
+  const prompt = body.prompt
+
+  try {
+    console.log('Calling OpenAI API')
+    const image = await openai.images.generate({
+      model: 'dall-e-3',
+      prompt: prompt,
+    })
+    console.log('OpenAI API response received')
+
+    const imageUrl = image.data[0].url
+    const finalData = image.data
+
+    user.currentCredits = user.currentCredits - 1
+    await user.save()
+
+    return NextResponse.json({ imageUrl: finalData })
+  } catch (error) {
+    console.error('OpenAI API Error:', error)
+
+    return NextResponse.json({ error: error.message }, { status: 500 })
+  }
+}
