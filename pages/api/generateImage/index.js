80c7cc2a78b52e988e40f97bc5b8812be8cfd5b7
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-08 13:45:15 -0500

    now>

--- pages/api/generateImage/index.js
+++ pages/api/generateImage/index.js
@@ -3,10 +3,7 @@
 import { json } from "express";
 import OpenAI from "openai";
 
-import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/libs/next-auth";
-import connectMongo from "@/libs/mongoose";
-import User from "@/models/User";
+
 
 const openai = new OpenAI();
 
@@ -16,16 +13,6 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method Not Allowed" });
   }
 
-  const session = await getServerSession(authOptions);
-
-  const { id } = session.user;
-
-  await connectMongo();
-
-  const user = await User.findById(id);
-  if (!user || user.currentCredits === 0) {
-    res.status(403).json({ error: "Not enough credits" });
-  }
 
   const { prompt } = req.body;
   console.log(req.body);
@@ -44,8 +31,7 @@ export default async function handler(req, res) {
     const imageUrl = image.data[0].url;
     const finalData = image.data;
 
-    user.currentCredits = user.currentCredits - 1;
-    await user.save();
+   
 
     res.status(200).json({ imageUrl, finalData });
   } catch (error) {
