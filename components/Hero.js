e66bd2bedfefcb4cbd1a7522522e7db6d9a4c617
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-09 15:48:27 -0500

    done

--- components/Hero.js
+++ components/Hero.js
@@ -10,15 +10,15 @@ function Hero() {
     <section className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-5 lg:gap-20 px-8 py-8 lg:py-20">
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
        
-    
-
-      <h1 class="bg-clip-text text-transparent bg-gradient-to-br from-[#007880] to-[#9C009F] leading-tight">Fire your Tattoo Designer</h1>
+        <h1 className="font-medium font-sans-serif text-4xl lg:text-6xl tracking-tight md:-mb-4 text-align:left">
+          <span class="bg-clip-text text-transparent bg-gradient-to-br from-[#007880] to-[#9C009F] leading-tight">Fire your Tattoo Designer</span>
+        </h1>
         <p className="font-medium text-lg opacity-80 leading-relaxed text-black">
-          Custom Tattoo Designs printed from the comfort of your home
+          Custom Tattoo Designs from the comfort of your home within seconds
         </p>
 
         <p className="font-medium ">
-          Transform any tattoo idea into a custom tattoo for less than 3x the price from a Designer
+          Transform any tattoo idea into a unique piece that will keep your artist interested during the session
         </p>
         <SigninButton className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3"></SigninButton>
         <p className="text-black text-align:left opacity-80"></p>
