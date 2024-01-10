b2a09e93e41f5d00a60dbcf663dedccb1ba90be9
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-09 22:29:29 -0500

    g

--- app/page.js
+++ app/page.js
@@ -183,7 +183,7 @@ export default function Home() {
           </h3>
 
           <Image
-          className="w-screen max-w-[16rem]"
+          className="w-screen max-w-[20rem]"
             src="/media/IMG_5415 Small.png"
             alt="tattoo"
             priority={true}
