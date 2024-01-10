b70f44cf5e0be4d573a7da35dc6bb4a6cb33bd19
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-09 19:44:07 -0500

    done

--- components/Hero.js
+++ components/Hero.js
@@ -11,13 +11,13 @@ function Hero() {
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
        
         <h1 className="font-medium font-sans-serif text-4xl lg:text-6xl tracking-tight md:-mb-4 text-align:left">
-          <span class="bg-clip-text text-transparent bg-gradient-to-br from-[#007880] to-[#9C009F] leading-tight">Spend less time Designing, more on inking with AI</span>
+          <span class="bg-clip-text text-transparent bg-gradient-to-br from-[#007880] to-[#9C009F] leading-tight">Spend less time designing, more on inking with AI</span>
         </h1>
         <p className="font-medium text-lg opacity-80 leading-relaxed text-black">
        Forget Designing, Spend that time to Focus on showing off your talent to clients
         </p>
-        No more stress for your client&apos;s next session
         <p className="font-medium ">
+        No more stress for your client&apos;s next session
         </p>
         <SigninButton className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3"></SigninButton>
         <p className="text-black text-align:left opacity-80"></p>
