1176502e9c97dcb4f859164553dd70754ffeb1a8
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-10 18:34:44 -0500

    done

--- components/Hero.js
+++ components/Hero.js
@@ -7,7 +7,14 @@ const priceId = config.stripe.plans[0].priceId; // Assuming it's the first plan
 
 function Hero() {
   return (
-    <section className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-5 lg:gap-20 px-8 py-8 lg:py-20">
+    <section className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-5 lg:gap-20 px-8 py-8 lg:py-20"
+    style={{ 
+      backgroundImage: "url('/media/war Small.jpeg')",
+      backgroundSize: 'cover', // Ensures the image covers the whole section
+      backgroundPosition: 'center', // Centers the image
+      backgroundRepeat: 'no-repeat' // Prevents the image from repeating
+    }}
+    >
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
         <h1 className="font-medium font-sans-serif text-4xl lg:text-6xl tracking-tight md:-mb-4 text-align:left">
           <span class="bg-clip-text text-transparent bg-gradient-to-br from-[#007880] to-[#9C009F] leading-tight">
