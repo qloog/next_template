8d3d63b6591e0c30a0d1705385c466868a5f8674
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-09 22:53:35 -0500

    dd

--- app/page.js
+++ app/page.js
@@ -116,7 +116,7 @@ export default function Home() {
               </defs>
             </svg>
             <p class="font-medium text-center">
-              Partnered with 97 artists worldwide
+              Used by 1k+ tattooed rebels
             </p>
             <svg
               xmlns="http://www.w3.org/2000/svg"
