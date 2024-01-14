8d205f37943c5dabca1992ac5a5b66df00c00183
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-14 00:04:09 -0500

    d

--- app/page.js
+++ app/page.js
@@ -122,36 +122,6 @@ export default function Home() {
                 </linearGradient>
               </defs>
             </svg>
-            <p class="font-medium text-center text-white">
-              1k+ customers find the right custom design for them 
-            </p>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              viewBox="0 0 24 24"
-            >
-              <g>
-                <path
-                  fill="url(#paint0_linear_3_2235)"
-                  d="M22.86 7.143a1.133 1.133 0 00-1.25-.16l-4.708 2.344-3.93-6.528a1.135 1.135 0 00-1.944 0L7.098 9.33 2.392 6.986a1.135 1.135 0 00-1.584 1.37l3.469 10.627a.75.75 0 001.095.412C5.395 19.381 7.792 18 12 18c4.207 0 6.604 1.381 6.626 1.394a.75.75 0 001.098-.411l3.469-10.625a1.131 1.131 0 00-.333-1.215zM16.485 14.7a.75.75 0 01-.87.609 21.396 21.396 0 00-7.239 0 .75.75 0 11-.26-1.478 22.917 22.917 0 017.76 0 .75.75 0 01.612.87h-.003z"
-                ></path>
-              </g>
-              <defs>
-                <linearGradient
-                  id="paint0_linear_3_2235"
-                  x1="12.002"
-                  x2="12.002"
-                  y1="2.25"
-                  y2="19.5"
-                  gradientUnits="userSpaceOnUse"
-                >
-                  <stop stop-color="#2A3966"></stop>
-                  <stop offset="1" stop-color="#5C3293"></stop>
-                </linearGradient>
-              </defs>
-            </svg>
           </div>
           <h3 className="font-semibold text-base-content text-lg opacity-80 text-white">
             Generate Your Tattoo
