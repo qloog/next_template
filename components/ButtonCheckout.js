fdeb833ba2596034d4db5f39905912b734b2cfa4
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-13 23:36:36 -0500

    ?

--- components/ButtonCheckout.js
+++ components/ButtonCheckout.js
@@ -32,7 +32,7 @@ const ButtonCheckout = ({ priceId, mode = "payment" }) => {
 
   return (
     <button
-      className="btn bg-gray text-white w-full max-w-xs space-y-3"
+      className="btn bg-orangered text-white w-full max-w-xs space-y-3"
       onClick={() => handlePayment()}
     >
       {isLoading ? (
