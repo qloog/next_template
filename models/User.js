2fe6a3bfaac1e2789017a0372dfe35a726a9dc5e
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-16 11:33:10 -0500

    j

--- models/User.js
+++ models/User.js
@@ -43,7 +43,7 @@ const userSchema = mongoose.Schema(
     planType: {
       type: String,
       enum: ['beginner', 'veteran', 'premium'],
-      default: 'beginner',
+      default: 'freePlan',
     },
   },
 
