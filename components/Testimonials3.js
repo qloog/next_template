8d1ccabdbc5e9f72715d5bdcac81eb040617c734
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-17 12:27:38 -0500

    done

--- components/Testimonials3.js
+++ components/Testimonials3.js
@@ -80,7 +80,7 @@ const Testimonials3 = () => {
       <div className="py-24 px-8 max-w-7xl mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <div className="mb-8">
-            <h2 className="sm:text-5xl text-4xl font-extrabold text-base-content">
+            <h2 className="sm:text-5xl text-4xl font-extrabold text-base-content text-white">
             Customers can&apos;t stop raving about the tattoos they generated
             </h2>
           </div>
