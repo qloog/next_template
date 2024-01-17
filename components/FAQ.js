3934f104e0c823499869e10b9bc072e4c14b28e6
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-17 12:58:32 -0500

    done

--- components/FAQ.js
+++ components/FAQ.js
@@ -136,7 +136,7 @@ const FAQ = () => {
       <div className="py-24 px-8 max-w-7xl mx-auto flex flex-col md:flex-row gap-12">
         <div className="flex flex-col text-left basis-1/2">
           <p className="inline-block font-semibold text-primary mb-4">FAQ</p>
-          <p className="sm:text-4xl text-3xl font-extrabold text-base-content">
+          <p className="sm:text-4xl text-3xl font-extrabold text-base-content text-white">
             Frequently Asked Questions
           </p>
         </div>
