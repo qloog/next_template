43d833f18dd118af72c11603dba59ffe032b6ba1
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-17 11:52:56 -0500

    d

--- components/TestimonialRating.js
+++ components/TestimonialRating.js
@@ -31,7 +31,7 @@ const TestimonialRating = () => {
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
-                fill="currentColor"
+                fill="white"
                 className="w-4 h-4 text-yellow-500"
               >
                 <path
