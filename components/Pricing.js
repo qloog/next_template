36c636388107c3b35675be18fca1e7de6bfae6a1
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-17 13:34:10 -0500

    d

--- components/Pricing.js
+++ components/Pricing.js
@@ -12,7 +12,7 @@ const Pricing = () => {
         <div className="flex flex-col text-center w-full mb-20">
           <p className="font-medium text-primary mb-8">Pricing</p>
           <h2 className="font-bold text-3xl lg:text-5xl tracking-tight">
-            Save hours of repetitive code and ship faster!
+            Save on custom designs with us, shine with your artist&apos;s ink
           </h2>
         </div>
 
