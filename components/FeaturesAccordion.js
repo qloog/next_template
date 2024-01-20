e72640ce564704e2a7022fd596851cbc1ac90f3a
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-20 17:45:38 -0500

    d

--- components/FeaturesAccordion.js
+++ components/FeaturesAccordion.js
@@ -16,8 +16,8 @@ const features = [
     description:
       "Render unqiue tattoos that nobody has, bring 'hard to draw' designs to a reality, and show off a compliment magnet",
     //type: "video",
-    //path: //"https://d3m8mk7e1mf7xn.cloudfront.net/app/newsletter.webm"//,
-    //format: "video/webm",//
+    path: "https://rajvirnahar6667.wistia.com/medias/8auqdzehww",
+    format: "video/webm",
   },
   {
     title: "Credit-based payment",
