c8be2162ba29b4b164b5014530dd3b94d632bdbd
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-21 21:18:54 -0500

    d

--- components/Hero.js
+++ components/Hero.js
@@ -10,12 +10,12 @@ function Hero() {
   return (
     <section
       className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center justify-center gap-5 lg:gap-20 px-8 py-8 lg:py-20 bg-black"
-      style={{
-        backgroundImage: "url('/media/photo-1568515045052-f9a854d70bfd.avif')",
-        backgroundSize: "cover", // Ensures the image covers the whole section
-        backgroundPosition: "center", // Centers the image
-        backgroundRepeat: "no-repeat", // Prevents the image from repeating
-      }}
+      //style={{
+        //backgroundImage: "url('/media/photo-1568515045052-f9a854d70bfd.avif')",
+        //backgroundSize: "cover", // Ensures the image covers the whole section
+        //backgroundPosition: "center", // Centers the image
+        //backgroundRepeat: "no-repeat", // Prevents the image from repeating
+     // }}
     >
       <div className="flex flex-col gap-10 lg:gap-14 items-center justify-center text-left lg:text-left lg:items-start">
         <h1 className="text-white font-medium font-sans-serif text-4xl lg:text-6xl tracking-tight md:-mb-4 text-align:left">
