91a71a3375f59db627b79845419c62d0d5df7e47
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-23 13:24:56 -0500

    d

--- components/ButtonSignIn.js
+++ components/ButtonSignIn.js
@@ -6,7 +6,7 @@ import config from "@/config";
 const SigninButton = () => {
   return (
     <button
-    className="btn btn-gradient animate-shimmer w-full max-w-xs space-y-3"
+    className="btn btn-primary btn-block w-full max-w-xs space-y-3"
       onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
     >
      Start designing - it&apos;s free
