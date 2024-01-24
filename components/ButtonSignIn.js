0361486f21704dd5745a9dcfb3b06914f2cde589
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-23 22:00:19 -0500

    d

--- components/ButtonSignIn.js
+++ components/ButtonSignIn.js
@@ -6,7 +6,10 @@ import config from "@/config";
 const SigninButton = () => {
   return (
     <button
-    className="btn btn-primary btn-block w-full max-w-xs space-y-3"
+    className="btn btn-block w-full max-w-xs space-y-3"
+    style={{
+      backgroundColor: 'rgb(50, 61, 214)'
+    }}
       onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
     >
      Start designing - 2 free credits
