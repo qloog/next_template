ed031a6d80601879982354be45b3cb0352853b67
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-23 22:01:36 -0500

    h

--- components/ButtonSignIn.js
+++ components/ButtonSignIn.js
@@ -6,7 +6,7 @@ import config from "@/config";
 const SigninButton = () => {
   return (
     <button
-    className="btn btn-block w-full max-w-xs space-y-3"
+    className="btn btn-block w-full max-w-xs space-y-3 text-white"
     style={{
       backgroundColor: 'rgb(50, 61, 214)'
     }}
