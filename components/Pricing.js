4d49f08d1485e6c17b29d588d75b4f5d9d543544
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-01-31 15:08:35 -0500

    d

--- components/Pricing.js
+++ components/Pricing.js
@@ -11,7 +11,7 @@ const Pricing = () => {
       <div className="py-24 px-8 max-w-5xl mx-auto">
         <div className="flex flex-col text-center w-full mb-20">
           <p className="font-medium text-primary mb-8">Pricing</p>
-          <h2 className="font-bold text-3xl lg:text-5xl tracking-tight"
+          <h2 className=" text-3xl lg:text-5xl tracking-tight"
           style={{
             fontFamily: "'EB Garamond', serif",
           }}>
