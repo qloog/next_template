4f40b491dfac33a29a89be938ec8d88988e20420
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-01 14:40:30 -0500

    ccc

--- components/CTA.js
+++ components/CTA.js
@@ -4,32 +4,39 @@ import config from "@/config";
 const CTA = () => {
   return (
     <section className="relative hero text-black bg-white min-h-screen">
-      
       <div className="relative hero-overlay bg-white"></div>
       <div className="relative hero-content text-black text-neutral-content p-8">
         <div className="flex flex-col items-center max-w-xl p-8 md:p-0">
-          <h2 className=" text-3xl md:text-5xl tracking-tight mb-8 md:mb-12 text-align:left text-black"
-          style={{
-            fontFamily: "'EB Garamond', serif",
-          }}>
-          Try TattooswithAI for free today
-
+          <h2
+            className=" text-3xl md:text-5xl tracking-tight mb-8 md:mb-12 text-align:left text-black"
+            style={{
+              fontFamily: "'EB Garamond', serif",
+            }}
+          >
+            Try TattooswithAI for free today
           </h2>
-          <p className="text-lg opacity-80 mb-12 md:mb-16 text-align:left text-black"
-          style={{
-            fontFamily: "'EB Garamond', serif",
-          }}>
-          Create your first custom piece with TattooswithAI today and never look back
+          <p
+            className="text-lg opacity-80 mb-12 md:mb-16 text-align:left text-black"
+            style={{
+              fontFamily: "'EB Garamond', serif",
+            }}
+          >
+            Create your first custom piece with TattooswithAI today and never
+            look back
           </p>
 
           <button
             className="btn btn-block w-full max-w-xs space-y-3 text-white"
             style={{
-              backgroundColor: 'rgb(50, 61, 214)'
+              fontFamily: "'Inter', sans-serif", // Use the Inter font
+              fontWeight: 600,
+              backgroundColor: "rgb(50, 61, 214)",
             }}
-            onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
+            onClick={() =>
+              signIn(undefined, { callbackUrl: config.auth.callbackUrl })
+            }
           >
-             Sign up for a free credit
+            Sign up for a free credit
           </button>
         </div>
       </div>
