8bf93978a2993abf3ea5716139a7c6efccff6a43
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-01 22:13:39 -0500

    d

--- components/Footer.js
+++ components/Footer.js
@@ -76,7 +76,19 @@ const Footer = () => {
             </Link>
 
             <p className="mt-3 text-sm text-white">{config.appDescription}</p>
-            <ButtonSignIn></ButtonSignIn>
+            <button
+            className="btn btn-block w-full max-w-xs space-y-3 text-white"
+            style={{
+              fontFamily: "'Inter', sans-serif", // Use the Inter font
+              fontWeight: 600,
+              backgroundColor: "rgb(50, 61, 214)",
+            }}
+            onClick={() =>
+              signIn(undefined, { callbackUrl: config.auth.callbackUrl })
+            }
+          >
+            Sign up for a free credit
+          </button>
             <p className="mt-3 text-sm text-white">
               Copyright © {new Date().getFullYear()} - All rights reserved
             </p>
