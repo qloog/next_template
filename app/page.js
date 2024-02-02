2bf2e4d2f1fc6d1bef2ad75aa185af334634ec0e
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-02 13:44:36 -0500

    dd

--- app/page.js
+++ app/page.js
@@ -203,7 +203,7 @@ export default function Home() {
         <Hero />
         <section
           id="tattoo-generator"
-          className="bg-white text-black"
+          className="max-w-7xl mx-auto flex flex-col lg:flex-row text-left gap-16 lg:gap-20 px-8 py-8 lg:py-20 bg-white text-black"
         >
           
           <h3
