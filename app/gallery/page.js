b84ec6a40b7797befd83d9f7a312a289eee9fca4
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-08 14:57:17 -0500

    dd

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 import logo from "@/app/icon.png";
 import config from "@/config";
 
+
 // Dummy data for gallery images
 const galleryImages = [
   { id: 1, url: '/media/tiger.webp', alt: 'Tattoo Design 1' },
@@ -23,10 +24,39 @@ const galleryImages = [
   { id: 15, url: '/media/Khalsa Small.jpeg', alt: 'Tattoo Design 2' },
   
 ];
+const logo = '/app/icon.png';
 
 const page = () => {
   return (
     <div style={{ padding: '20px', backgroundColor: '#fff' }}>
+         {/* Header with logo and app name */}
+      <div className="flex lg:flex-1">
+        <Link href="/" className="flex items-center gap-2 shrink-0" title={`${config.appName} homepage`}>
+          <Image src={logo} alt={`${config.appName} logo`} className="w-8" placeholder="blur" priority={true} width={32} height={32} />
+          <div>
+            {/* Inline style for the animated text */}
+            <style jsx>{`
+              @keyframes animatedgradient {
+                0% { background-position: 0% 50%; }
+                50% { background-position: 100% 50%; }
+                100% { background-position: 0% 50%; }
+              }
+              .animated-text {
+                font-size: 24px;
+                font-weight: bold;
+                background: linear-gradient(60deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82);
+                background-size: 300% 300%;
+                -webkit-background-clip: text;
+                -webkit-text-fill-color: transparent;
+                animation: animatedgradient 6s ease infinite alternate;
+              }
+            `}</style>
+            <span className="font-extrabold text-lg">
+              <span className="animated-text">{config.appName}</span>
+            </span>
+          </div>
+        </Link>
+        </div>
       <h1 style={{ color: 'black', fontFamily: "'Poppins', sans-serif", textAlign: 'center', marginBottom: '20px' }}>Latest Design Renders</h1>
       <div style={{
         display: 'grid',
