03945d5a06850de51c1ba210be5da2b14fde7679
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-09 14:06:12 -0500

    jj

--- app/api/generateImage/route.js
+++ app/api/generateImage/route.js
@@ -47,30 +47,6 @@ export async function POST(req) {
     user.currentCredits = user.currentCredits - 1
     await user.save()
 
-    if (body.includeInGallery) {
-      // Assuming `imageUrl` contains the URL received from OpenAI
-      const imageUrl = finalData; // Or however you extract the URL from the response
-    
-      // Create a simple `alt` text based on the user's prompt, or use a generic description
-      const altText = `Tattoo design inspired by user's prompt: ${prompt}`; // or a generic description
-    
-      // Create a new gallery item object to save to your database
-      const galleryItem = {
-        userId: user._id, // Assuming you have the user's ID from the session or another source
-        url: imageUrl,
-        alt: altText,
-      };
-    
-      // Save this object to your database
-      try {
-        // Assuming you have a function or method to save to your gallery collection
-        await saveGalleryItem(galleryItem);
-      } catch (error) {
-        console.error('Failed to save image to gallery:', error);
-        // Optionally return an error response
-      }
-    }
-    
 
 
     return NextResponse.json({ imageUrl , finalData })
