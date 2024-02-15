0274fee8215c73c1c5808b336c7662c9197d696a
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-15 13:08:08 -0500

    d

--- app/api/editUserImage/route.js
+++ app/api/editUserImage/route.js
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { Configuration, OpenAIApi } from "openai";
+import { Configuration, OpenAIApi } from "openai-edge"
 
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY2,
