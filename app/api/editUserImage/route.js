ea6c30a133ed970b9a7436d0f162dc6c71ecf22d
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-16 13:38:01 -0500

    jd dj

--- app/api/editUserImage/route.js
+++ app/api/editUserImage/route.js
@@ -1,29 +1,38 @@
-import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from "ai"
+import { model } from "mongoose";
+import { Configuration, OpenAIApi } from "openai-edge"
 
-import { OpenAIStream, StreamingTextResponse} from 'ai'
+export const runtime = 'edge'
 
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY2
+})
 
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY2,
-});
+const openai = new OpenAIApi(configuration);
 
 export async function POST(req) {
+const { image } = await req.json()
 
-const { content } = await req.json()
-
-
-const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "Say this is a test" }],
+const response = await openai.createChatCompletion({
     model: "gpt-4-vision-preview",
     stream: true,
+    max_tokens: 4096,
+    messages: [
+        {
+            role: "user",
+            content: [
+                { type: "text", text: "Modify the image to a better styling" }, 
+                { type: "image_url", image_url: image }//base64 images
+            ]
+        }
+    ]
 });
+const stream = OpenAIStream(response);
 
-const stream = OpenAIStream(chatCompletion)
+return new StreamingTextResponse(stream);
 
 
-return new StreamingTextResponse(stream)
 }
-
 
 
 
