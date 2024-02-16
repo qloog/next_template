7e4bf2ed9197c8e0147c8a426219d5efc8a30722
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-16 13:13:58 -0500

    d

--- app/api/editUserImage/route.js
+++ app/api/editUserImage/route.js
@@ -1,36 +1,43 @@
-import { OpenAIStream, StreamingTextResponse } from "ai"
 import { Configuration, OpenAIApi } from "openai-edge"
+import { OpenAIStream, StreamingTextResponse } from "ai"
 
-export const runtime = 'edge'
+export const runtime = 'edge';
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_API_KEY2
-})
+    apiKey: "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"
+});
 
 const openai = new OpenAIApi(configuration);
 
-export async function POST(req) {
-const { image } = await req.json()
+// Route Handlers let us create API logic
+// POST api/analyzeImage
+export async function POST(request) {
+    // { image: "ASDFASDFASDF base64 string" }
+    const { image } = await request.json();
 
-const response = await openai.createChatCompletion({
-    model: "gpt-4-vision-preview",
-    stream: true,
-    max_tokens: 4096,
-    messages: [
-        {
-            role: "user",
-            content: [
-                { type: "text", text: "Modify the image to a better styling" }, 
-                { type: "image_url", image_url: image }//base64 images
-            ]
-        }
-    ]
-});
-const stream = OpenAIStream(response);
+    const response = await openai.createChatCompletion({
+        model: "gpt-4-vision-preview",
+        stream: true,
+        max_tokens: 4096, // No max tokens: super short / cut off response.
+        messages: [ // GPT-4 with Vision is JUST GPT-4. So you can still talk with it like GPT-4
+            // There is no "system" message (THIS MAY CHANGE)
+            {
+                role: "user",
+                //@ts-ignore
+                content: [
+                    { type: "text", text: "What's in this image?" },
+                    {
+                        type: "image_url",
+                        image_url: image // base64 images
+                    }
+                ]
+            }
+        ]
+    });
 
-return new StreamingTextResponse(stream);
+    const stream = OpenAIStream(response);
 
-
+    return new StreamingTextResponse(stream);
 }
 
 
