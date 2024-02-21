c07c3e72ed11dd085e55a1d38e4f24b42f953d3a
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-21 12:14:59 -0500

    j

--- app/api/generateWithDalle/route.js
+++ app/api/generateWithDalle/route.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
       }
     } else {
       res.setHeader('Allow', ['POST']);
-      res.status(405).end('Method Not Allowed');
+      return res.status(405).end('Method Not Allowed');
     }
   }
   
