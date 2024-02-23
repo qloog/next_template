bddb2f3685a5d2534ead5cb5a3735558d576d785
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-23 14:13:02 -0500

    d

--- app/api/generateImage/route.js
+++ app/api/generateImage/route.js
@@ -1,3 +1,6 @@
+export const maxDuration = 120
+export const dynamic = "force-dynamic"
+
 import { NextResponse } from 'next/server'
 import OpenAI from 'openai'
 import { getServerSession } from "next-auth/next";
