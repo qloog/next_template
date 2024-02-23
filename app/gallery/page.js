736fc3b00542bcfb0dcf3f1bfe05512f7a5c97be
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-23 13:36:38 -0500

    do e

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -1,65 +1,4 @@
-"use client"
 
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
-
-// Initialize Supabase client
-const supabaseClient = createClient(
-  process.env.SUPABASE_PROJECT_URL,
-  process.env.SUPABASE_API_KEY
-);
-
-export default function Gallery() {
-  const [images, setImages] = useState([]);
-
-  useEffect(() => {
-    async function fetchImages() {
-      const { data, error } = await supabaseClient
-        .from('images')
-        .select('*')
-        .order('id');
-
-      if (error) {
-        console.error('Error fetching images:', error);
-      } else {
-        setImages(data);
-      }
-    }
-
-    fetchImages();
-  }, []);
-
-  return (
-    <div className="mx-auto max-w-2xl py-16 px-4 sm:py-24 sm:px-6 lg:max-w-7xl lg:px-8">
-      <div className="grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        {images.map((image) => (
-          <a key={image.id} href={image.href} className="group">
-            <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
-              <Image
-                alt={image.name}
-                src={image.imageSrc}
-                layout="fill"
-                objectFit="cover"
-                className="duration-700 ease-in-out group-hover:opacity-75"
-              />
-            </div>
-            <h3 className="mt-4 text-sm text-gray-700">{image.name}</h3>
-            <p className="mt-1 text-lg font-medium text-gray-900">{image.username}</p>
-          </a>
-        ))}
-      </div>
-    </div>
-  );
-}
-
-
-
-
-
-
-
-  /*
 export default function Gallery() {
     
     const galleryImages = [
@@ -116,5 +55,5 @@ export default function Gallery() {
   }
 
 
-  */
+  
   
