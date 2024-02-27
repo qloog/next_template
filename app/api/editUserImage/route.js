67004e55d76f85f15efa8a787898808e438cfd7e
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-02-27 12:29:57 -0500

    d

--- app/api/editUserImage/route.js
+++ app/api/editUserImage/route.js
@@ -30,7 +30,6 @@ export default async function handler(req, res) {
       });
 
       // Log the response and send it back to the client
-      console.log(response.choices[0]);
       res.status(200).json(response.choices[0]);
     } catch (error) {
       console.error(error);
