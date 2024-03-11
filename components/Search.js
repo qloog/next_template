4a75ac8e153d64484c535fa79b4c06a763b61b3c
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-11 12:48:43 -0400

    new

--- components/Search.js
+++ components/Search.js
@@ -1,25 +1,23 @@
 import { useState } from 'react';
-import { useRouter } from 'next/router';
 
-export default function Search() {
-    const [search, setSearch] = useState('');
-    const router = useRouter();
+export default function Search({ onSearch }) {
+  const [search, setSearch] = useState('');
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        router.push(`/results/${search}`);
-        setSearch('');
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSearch(search);
+    setSearch('');
+  };
 
-    return (
-        <form className="flex justify-center md:justify-between" onSubmit={handleSubmit}>
-            <input
-                type="text"
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-                placeholder='Search'
-                className="bg-white p-2 w-[260px] sm:w-80 text-xl rounded-xl text-black"
-            />
-        </form>
-    );
+  return (
+    <form className="flex justify-center md:justify-between" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder='Search'
+        className="bg-white p-2 w-[260px] sm:w-80 text-xl rounded-xl text-black"
+      />
+    </form>
+  );
 }
