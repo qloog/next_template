02584f4fd2b8d3ab74064c0e25353ed3339a50aa
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 12:26:50 -0400

    d

--- components/GalleryLogin.js
+++ components/GalleryLogin.js
@@ -7,11 +7,6 @@ const GalleryButton = () => {
   return (
     <button
       className="btn w-full max-w-xs space-y-3 text-white"
-      style={{
-        fontFamily: "'Poppins', sans-serif",
-        fontWeight: 600, 
-        backgroundColor: 'rgb(50, 61, 214)'
-      }}
       onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
     >
       View your design history
