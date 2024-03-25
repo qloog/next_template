7de08c82ffeb07ec5d5dabcfbba8e33dec70814b
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 13:15:41 -0400

    d

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -35,11 +35,6 @@ export default function Dashboard() {
 
     return (
         <main className="bg-white text-black min-h-screen p-8 pb-24">
-            <section className="max-w-xl mx-auto space-y-8" id="about">
-                <h1 className="text-black text-4xl lg:text-6xl tracking-tight md:-mb-4 text-align:left" style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 600 }}>
-                    Our mission is to usher in a new era of human creativity through artificial intelligence.
-                </h1>
-            </section>
             <section>
                 <h2 className="text-black text-4xl lg:text-6xl tracking-tight md:-mb-4 text-align:left" style={{ fontFamily: "'Poppins', sans-serif", fontWeight: 600 }}>
                     Your Uploaded Designs
@@ -58,7 +53,7 @@ export default function Dashboard() {
                         ))}
                     </div>
                 ) : (
-                    <p>You haven't uploaded any public designs yet.</p>
+                    <p>You haven&apos;t uploaded any public designs yet.</p>
                 )}
             </section>
             <style jsx>{`
