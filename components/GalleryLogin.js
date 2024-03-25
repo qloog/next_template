9458a93bad0891b4314fd34283ee7198f38df703
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 12:21:16 -0400

    d

--- components/GalleryLogin.js
+++ components/GalleryLogin.js
@@ -2,16 +2,12 @@
 
 import { signIn } from "next-auth/react";
 import config from "@/config";
+import "@/app/GalleryButton.css";
                 
 const GalleryButton = () => {
   return (
     <button
-      className="btn w-full max-w-xs space-y-3 text-white"
-      style={{
-        fontFamily: "'Poppins', sans-serif",
-        fontWeight: 600, 
-        backgroundColor: 'rgb(50, 61, 214)'
-      }}
+      className="btn w-full max-w-xs space-y-3 text-white galleryButton"
       onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
     >
       View your design history
