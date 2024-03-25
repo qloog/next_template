18baeb784fb950064c8f43ac270a87b569e9da56
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 15:11:03 -0400

    d

--- app/api/deleteDesign/route.js
+++ app/api/deleteDesign/route.js
@@ -1,32 +1,31 @@
-
-
 import connectMongo from "@/libs/mongoose";
 import { getSession } from 'next-auth/react';
 import Image from "@/models/Image";
+import { NextResponse } from 'next/server';
 
 export async function POST(req) {
     if (req.method !== 'DELETE') {
-        return res.status(405).json({ error: 'Method not allowed' });
+        return NextResponse.json({ error: 'Method not allowed' }, { status: 405 });
     }
 
     const session = await getSession({ req });
     if (!session || !session.user) {
-        return res.status(401).json({ error: 'Unauthorized' });
+        return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
     }
 
-    const designId = req.query.designId;
+    const { designId } = req.query;
 
     await connectMongo();
 
     try {
         const deletedDesign = await Image.findOneAndDelete({ _id: designId, uploaderId: session.user.id });
         if (!deletedDesign) {
-            return res.status(404).json({ error: 'Design not found' });
+            return NextResponse.json({ error: 'Design not found' }, { status: 404 });
         }
 
-        return res.status(200).json({ message: 'Design deleted successfully' });
+        return NextResponse.json({ message: 'Design deleted successfully' }, { status: 200 });
     } catch (error) {
         console.error('Error deleting design:', error);
-        return res.status(500).json({ error: 'Internal server error' });
+        return NextResponse.json({ error: 'Internal server error' }, { status: 500 });
     }
 }
