aa7ad61adaacad670901e7fe4d3152ea89030d38
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 12:07:59 -0400

    d

--- components/GalleryLogin.js
+++ components/GalleryLogin.js
@@ -1,61 +1,24 @@
-"use client";
+"use client"
 
-import { useSession, signIn } from "next-auth/react";
-import Link from "next/link";
-import { useRouter } from 'next/navigation';
+import { signIn } from "next-auth/react";
 import config from "@/config";
-
-// A simple button to sign in with our providers (Google & Magic Links).
-// It automatically redirects user to callbackUrl (config.auth.callbackUrl) after login, which is normally a private page for users to manage their accounts.
-// If the user is already logged in, it will show their profile picture & redirect them to callbackUrl immediately.
-const ButtonSignin = ({ extraStyle, callbackUrl }) => {
-  const router = useRouter();
-  const { data: session, status } = useSession();
-
-  const handleClick = () => {
-    const redirectTo = callbackUrl || config.auth.callbackUrl;
-    if (status === "authenticated") {
-      // Check if the router is ready before using it
-      if (router.isReady) {
-        router.push(redirectTo);
-      }
-    } else {
-      signIn(undefined, { callbackUrl: redirectTo });
-    }
-  };
-
-  if (status === "authenticated") {
-    return (
-      <Link
-        href={config.auth.callbackUrl}
-        className={`btn ${extraStyle ? extraStyle : ""}`}
-      >
-        {session.user?.image ? (
-          <img
-            src={session.user?.image}
-            alt={session.user?.name || "Account"}
-            className="w-6 h-6 rounded-full shrink-0"
-            referrerPolicy="no-referrer"
-            width={24}
-            height={24}
-          />
-        ) : (
-          <span className="w-6 h-6 bg-base-300 flex justify-center items-center rounded-full shrink-0">
-            {session.user?.name?.charAt(0) || session.user?.email?.charAt(0)}
-          </span>
-        )}
-        {session.user?.name || session.user?.email || "Account"}
-      </Link>
-    );
-  }
-
+                
+const SigninButton = () => {
   return (
     <button
-      className={`btn ${extraStyle ? extraStyle : ""}`}
-      onClick={handleClick}
+    className="btn w-full max-w-xs space-y-3 text-white"
+    style={{
+    fontFamily: "'Poppins', sans-serif", // Use the Inter font
+    fontWeight: 600, 
+      backgroundColor: 'rgb(50, 61, 214)'
+    }}
+      onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
     >
+     Sign up for 2 free credits
     </button>
+    
   );
 };
 
-export default ButtonSignin;
+                
+export default SigninButton;
