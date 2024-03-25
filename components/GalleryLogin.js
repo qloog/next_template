7b97664439d6ee0b8b5571e7aa9edbc5888e06d8
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 12:29:07 -0400

    d

--- components/GalleryLogin.js
+++ components/GalleryLogin.js
@@ -1,3 +1,5 @@
+
+/*
 "use client";
 
 import { signIn } from "next-auth/react";
@@ -7,6 +9,11 @@ const GalleryButton = () => {
   return (
     <button
       className="btn w-full max-w-xs space-y-3 text-white"
+      style={{
+        fontFamily: "'Poppins', sans-serif",
+        fontWeight: 600, 
+        backgroundColor: 'rgb(50, 61, 214)'
+      }}
       onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
     >
       View your design history
@@ -15,3 +22,4 @@ const GalleryButton = () => {
 };
 
 export default GalleryButton;
+*/
