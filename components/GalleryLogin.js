4c1572c1c3e7748964f4861d57eedb5989b0db30
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 12:10:50 -0400

    x

--- components/GalleryLogin.js
+++ components/GalleryLogin.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { signIn } from "next-auth/react";
 import config from "@/config";
@@ -6,19 +6,17 @@ import config from "@/config";
 const SigninButton = () => {
   return (
     <button
-    className="btn w-full max-w-xs space-y-3 text-white"
-    style={{
-    fontFamily: "'Poppins', sans-serif", // Use the Inter font
-    fontWeight: 600, 
-      backgroundColor: 'rgb(50, 61, 214)'
-    }}
+      className="btn w-full max-w-xs space-y-3 text-white"
+      style={{
+        fontFamily: "'Poppins', sans-serif",
+        fontWeight: 600, 
+        backgroundColor: 'rgb(50, 61, 214)'
+      }}
       onClick={() => signIn(undefined, { callbackUrl: config.auth.callbackUrl })}
     >
-     Sign up for 2 free credits
+      View your design history
     </button>
-    
   );
 };
 
-                
 export default SigninButton;
