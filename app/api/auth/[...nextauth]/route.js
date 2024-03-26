28ed428d1d4d5b5c62b72962f2cf594b7d2a0cfd
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-25 20:21:16 -0400

    d

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,7 @@
 import NextAuth from "next-auth";
 import { authOptions } from "@/libs/next-auth";
 
-const options = {
-  ...authOptions,
-  session: {
-    ...authOptions.session,
-    url: "https://www.tattooswithai.com/api/auth/session",
-  },
-};
-
-const handler = NextAuth(options);
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
