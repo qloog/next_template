9cffd7ed1ed2563721448ecbd1cee842b8eed057
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-26 16:06:39 -0400

    x

---
+++ app/api/DisplayInGalleryPublic/route.js
@@ -0,0 +1,24 @@
+import connectMongo from '@/libs/mongoose';
+import Image from '@/models/Image';
+
+export async function GET() {
+  await connectMongo();
+
+  try {
+    const images = await Image.find();
+    return new Response(JSON.stringify(images), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    return new Response(JSON.stringify({ error: 'Failed to fetch images' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+}
