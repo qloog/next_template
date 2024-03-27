9fd4cbecfbeb014aa9cdf59f46aa4314e4c00e44
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-27 13:36:57 -0400

    d

--- app/api/deleteImage/route.js
+++ app/api/deleteImage/route.js
@@ -1,4 +1,3 @@
-import { NextResponse } from 'next/server';
 import connectMongo from '@/libs/mongoose';
 import Image from '@/models/Image';
 import { getServerSession } from 'next-auth/next';
@@ -8,25 +7,53 @@ export const maxDuration = 120;
 export const dynamic = 'force-dynamic';
 
 export async function DELETE(req) {
-  await connectMongo();
-
-  const session = await getServerSession(req, authOptions);
-
-  if (!session || !session.user) {
-    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  if (req.method !== 'DELETE') {
+    return new Response(JSON.stringify({ error: 'Method Not Allowed' }), {
+      status: 405,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 
-  const body = await req.json();
-  const { imageId } = body;
+  await connectMongo();
+
+  const session = await getServerSession({ req }, authOptions);
+
+  if (!session || !session.user) {
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), {
+      status: 401,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  const { imageId } = await req.json();
 
   try {
     const deletedImage = await Image.findByIdAndDelete(imageId);
     if (!deletedImage) {
-      return NextResponse.json({ error: 'Image not found' }, { status: 404 });
+      return new Response(JSON.stringify({ error: 'Image not found' }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
     }
-    return NextResponse.json({ message: 'Image deleted successfully' }, { status: 200 });
+    return new Response(JSON.stringify({ message: 'Image deleted successfully' }), {
+      status: 200,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   } catch (error) {
     console.error('Error deleting image:', error);
-    return NextResponse.json({ error: 'Failed to delete image' }, { status: 500 });
+    return new Response(JSON.stringify({ error: 'Failed to delete image' }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
 }
