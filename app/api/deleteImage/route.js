34c22627bc2df20f25393a156c3a153027c17edd
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-03-27 13:05:29 -0400

    s

--- app/api/deleteImage/route.js
+++ app/api/deleteImage/route.js
@@ -1,34 +1,30 @@
-
-
-import { NextResponse } from 'next/server';
-import { getServerSession } from 'next-auth/next';
 import connectMongo from '@/libs/mongoose';
 import Image from '@/models/Image';
+import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/libs/next-auth';
 
 export const maxDuration = 120;
 export const dynamic = 'force-dynamic';
 
-
-export default async function handler(req) {
+export async function DELETE(req) {
   await connectMongo();
 
-  const session = await getServerSession(req, authOptions);
+  const session = await getServerSession({ req }, authOptions);
 
   if (!session || !session.user) {
-    return new NextResponse(JSON.stringify({ error: 'Unauthorized' }), { status: 401 });
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), { status: 401, headers: { 'Content-Type': 'application/json' } });
   }
 
-  const { imageId } = req.body;
+  const { imageId } = await req.json();
 
   try {
     const deletedImage = await Image.findByIdAndDelete(imageId);
     if (!deletedImage) {
-      return new NextResponse(JSON.stringify({ error: 'Image not found' }), { status: 404 });
+      return new Response(JSON.stringify({ error: 'Image not found' }), { status: 404, headers: { 'Content-Type': 'application/json' } });
     }
-    return new NextResponse(JSON.stringify({ message: 'Image deleted successfully' }), { status: 200 });
+    return new Response(JSON.stringify({ message: 'Image deleted successfully' }), { status: 200, headers: { 'Content-Type': 'application/json' } });
   } catch (error) {
     console.error('Error deleting image:', error);
-    return new NextResponse(JSON.stringify({ error: 'Failed to delete image' }), { status: 500 });
+    return new Response(JSON.stringify({ error: 'Failed to delete image' }), { status: 500, headers: { 'Content-Type': 'application/json' } });
   }
 }
