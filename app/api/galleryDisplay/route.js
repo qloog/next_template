9153cb349dbce84e2abdc7a8176aab41f3b6c717
Rajvir Nahar <Rajvirnahar6667@gmail.com>
2024-04-01 13:37:58 -0400

    d

--- app/api/galleryDisplay/route.js
+++ app/api/galleryDisplay/route.js
@@ -4,7 +4,7 @@ import Image from "@/models/Image";
 export const maxDuration = 120;
 export const dynamic = "force-dynamic";
 
-export async function POST({ request }) {
+export async function GET({ request }) {
   await connectMongo();
 
   const { page, limit, alreadyDisplayedIds } = await request.json();
